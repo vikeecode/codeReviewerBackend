3adaa2a66553d14c22ff0efddaf0f6229c28cb2d
vikas <vikeepandit178@gmail.com>
2025-05-16 23:32:44 +0530

    fix

--- src/app.js
+++ src/app.js
@@ -3,15 +3,22 @@ const aiRoutes = require('./routes/ai.routes');
 const cors = require('cors');
 
 const app = express()
+const allowedOrigins = ['http://localhost:5173', 'https://gyanicodereviewer.netlify.app'];
 app.use(cors({
-    origin: ['http://localhost:5173',"https://gyanicodereviewer.netlify.app"], // Replace with your frontend URL
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) return callback(null, true);
+        else{
+            callback(new Error('Not allowed by CORS'));
+        }
+    }, // Replace with your frontend URL
     methods: ['GET', 'POST'],
     credentials: true,
+    allowedHeaders: 'Content-Type, Authorization',
 }));
 //if not use express.json then body will be undefined
 app.use(express.json());
-app.get('/', (req, res)=>{
-   res.send('Hello World'); 
+app.get('/', (req, res) => {
+    res.send('Hello World');
 })
 app.use('/ai', aiRoutes);
 
