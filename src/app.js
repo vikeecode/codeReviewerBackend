0ede6927a5ca91998c31190b2c07d021fb77b8db
vikas <vikeepandit178@gmail.com>
2025-05-16 23:21:47 +0530

    working

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const app = express()
 app.use(cors({
-    origin: ['http://localhost:5173',"https://gyanicodereviewer.netlify.app/"], // Replace with your frontend URL
+    origin: ['http://localhost:5173',"https://gyanicodereviewer.netlify.app"], // Replace with your frontend URL
     methods: ['GET', 'POST'],
     credentials: true,
 }));
